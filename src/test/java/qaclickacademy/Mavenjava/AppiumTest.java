43a922a3b408e7248ceedfeafbd505b4a59173f3
61414 <sidhuraman653@yahoo.com>
2022-01-20 13:58:52 -0500

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid()
+	{
+    System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+	    System.out.println("IOSApp");
+
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/ReatAPITest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class ReatAPITest {
+
+		@Test
+		public void PostJira()
+		{
+		    System.out.println("PostJira");
+
+		}
+		
+		@Test
+		public void DeleteTwitter()
+		{
+		    System.out.println("DeleteTwitter");
+
+		}
+
+}
